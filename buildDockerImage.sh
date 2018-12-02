2767d9a362a06db6651a80d353cbcc7d3ff20799
aakanitt <aakanitt@gmail.com>
2018-12-02 19:20:30 +0000

    tsafdfsf

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,11 +5,23 @@ dockerImageName=$1
 dockerTagVersion=$2
 jenkinsWorkspace=$3
 dockerRegistry=$4
+buildFolder=k8s
 echo ""
 echo "dockerImageName" $dockerImageName
 echo "dockerTagVersion" $dockerTagVersion
 echo $jenkinsWorkspace
 echo "dockerRegistry" $dockerRegistry
 echo ""
+rm -rf $jenkinsWorkspace/$buildFolder
+mkdir $jenkinsWorkspace/$buildFolder
+cp -r $jenkinsWorkspace/* $jenkinsWorkspace/$buildFolder/
+cd $jenkinsWorkspace/$buildFolder/
+docker build --no-cache -t $dockerImageName:$dockerTagVersion .
+echo ""
+docker tag $dockerImageName:$dockerTagVersion $dockerRegistry:$dockerTagVersion
+echo ""
+docker push $dockerRegistry:$dockerTagVersion
+echo ""
 docker images
+echo ""
 aws ecr list-images --repository-name=pao/nginx
