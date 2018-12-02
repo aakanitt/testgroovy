ca43c8c2e20beef2ca21f1ae93bf619a90b14c51
aakanitt <aakanitt@gmail.com>
2018-12-02 16:25:10 +0000

    add buildDockerImage script

---
+++ buildDockerImage.sh
@@ -0,0 +1,12 @@
+#/bin/bash -e
+
+#ENV
+dockerImageName="${1,,}"
+dockerTagVersion=$2
+jenkinsWorkspace=$3
+dockerRegistry=$4
+
+echo "dockerImageName" $dockerImageName
+echo "dockerTagVersion" $dockerTagVersion
+echo $jenkinsWorkspace
+echo "dockerRegistry" $dockerRegistry
--- pao1.groovy
+++ pao1.groovy
@@ -1,6 +1,18 @@
 #!groovy
+env.project_name = "TestGroovy"
+env.app_name = "test-nginx"
+env.tag_version = env.app_name + "-" + env.BUILD_NUMBER
+env.ecr_repo = "878147804699.dkr.ecr.ap-southeast-1.amazonaws.com/pao/testjenkin"
+
 node {
    stage('Source') { // for display purposes
       git 'https://github.com/aakanitt/testgroovy.git'
+   }
+   stage('Build Docker Image'){
+            dir('.'){
+                    sh '''
+                      buildDockerImage.sh $app_name $tag_version ${WORKSPACE}/$project_name $ecr_repo
+                    '''
+            }
    }
 }
