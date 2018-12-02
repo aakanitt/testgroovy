65ee243c7113166e3b0969c60e16ce581bab4740
aakanitt <aakanitt@gmail.com>
2018-12-02 16:52:02 +0000

    tere

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,8 +5,10 @@ dockerImageName=$1
 dockerTagVersion=$2
 jenkinsWorkspace=$3
 dockerRegistry=$4
-
+echo ""
 echo "dockerImageName" $dockerImageName
 echo "dockerTagVersion" $dockerTagVersion
 echo $jenkinsWorkspace
 echo "dockerRegistry" $dockerRegistry
+echo ""
+aws ecr list-images --repository-name=pao/nginx
--- pao1.groovy
+++ pao1.groovy
@@ -2,7 +2,7 @@
 env.project_name = "testPipeline1"
 env.app_name = "test-nginx"
 env.tag_version = env.app_name + "-" + env.BUILD_NUMBER
-env.ecr_repo = "878147804699.dkr.ecr.ap-southeast-1.amazonaws.com/pao/testjenkin"
+env.ecr_repo = "878147804699.dkr.ecr.ap-southeast-1.amazonaws.com/pao/nginx"
 
 node {
    stage('Source') { // for display purposes
